b987fca8354809a7b8f44fddb886451d5183de53
velios <prostozapomnit@gmail.com>
2017-08-06 01:01:06 +0300

    Made meaningful names of variables

--- pprint_json.py
+++ pprint_json.py
@@ -2,15 +2,15 @@ import json
 
 
 def load_data(filepath):
-    f = open(filepath, 'r', encoding='utf-8')
-    return f.read()
+    file = open(filepath, 'r', encoding='utf-8')
+    return file.read()
 
 
 def pretty_print_json(data):
-    parsed = json.loads(data)
-    return json.dumps(parsed, sort_keys=True, indent=4, separators=(',', ': '))
+    raw_json_data = json.loads(data)
+    return json.dumps(raw_json_data, sort_keys=True, indent=4, separators=(',', ': '))
 
 
 if __name__ == '__main__':
-    data = load_data('test.json')
-    print(pretty_print_json(data))
+    json_data_from_file = load_data('test.json')
+    print(pretty_print_json(json_data_from_file))
