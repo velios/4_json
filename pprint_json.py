d6bfb5dcf2c53746dc25f7187239472955164b45
velios <prostozapomnit@gmail.com>
2017-08-08 20:50:47 +0300

    Add commandline argument parser

--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 
 def load_data(filepath):
@@ -8,10 +9,19 @@ def load_data(filepath):
 
 def pretty_print_json(input_json_data):
     unformatted_json_data = json.loads(input_json_data)
-    pretty_printed_json_data = json.dumps(unformatted_json_data, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+    pretty_printed_json_data = json.dumps(unformatted_json_data, sort_keys=True, indent=4,
+                                          separators=(',', ': '), ensure_ascii=False)
     print(pretty_printed_json_data)
 
 
 if __name__ == '__main__':
-    json_data_from_file = load_data('test.json')
+    parser_description = """
+    Скрипт на вход принимает путь до файла в котором
+    хранится JSON и выводит его содержимое в консоль в удобном для чтении виде.
+    Файл для примера можно скачать на https://data.mos.ru/
+    """
+    cmd_argument_parser = argparse.ArgumentParser(description=parser_description)
+    cmd_argument_parser.add_argument('filepath', help='Имя файла с неформатированным JSON', type=str)
+    cmd_arguments = cmd_argument_parser.parse_args()
+    json_data_from_file = load_data(cmd_arguments.filepath)
     pretty_print_json(json_data_from_file)
