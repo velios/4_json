29656848d98dd1cc4f0f5bd00f8ac87889b045c9
velios <prostozapomnit@gmail.com>
2017-08-09 07:52:31 +0300

    Fixed crash if not exist file issue

--- pprint_json.py
+++ pprint_json.py
@@ -1,17 +1,22 @@
 import json
 import argparse
+import os
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        print('Неверное имя или файл "{0}" не существет'.format(filepath))
+        return None
     with open(filepath, 'r', encoding='utf-8') as data_file:
         return data_file.read()
 
 
 def pretty_print_json(input_json_data):
-    unformatted_json_data = json.loads(input_json_data)
-    pretty_printed_json_data = json.dumps(unformatted_json_data, sort_keys=True, indent=4,
-                                          separators=(',', ': '), ensure_ascii=False)
-    print(pretty_printed_json_data)
+    if input_json_data:
+        unformatted_json_data = json.loads(input_json_data)
+        pretty_printed_json_data = json.dumps(unformatted_json_data, sort_keys=True, indent=4,
+                                              separators=(',', ': '), ensure_ascii=False)
+        print(pretty_printed_json_data)
 
 
 if __name__ == '__main__':
